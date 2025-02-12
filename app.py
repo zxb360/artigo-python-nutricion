d0c2722a36a2792f02a882310f9c22bef0d0a4a4
jaeder azevedo <zxb360@gmail.com>
2025-02-11 22:27:36 -0300

    iniciando primeira etapa do intelligence nutricion

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+import google.generativeai as genai
+import os
+from flask import Flask, render_template, request
+
+model = genai.GenerativeModel('gemini-pro',
+                              generation_config={"temperature": 0.5}
+                              )
 
 app = Flask(__name__)
+GOOGLE_API_KEY = os.environ.get('GEMINI_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    response = None
+    if request.method == "POST":
+        input = request.form['input']  # request.form.get('input')
+        if input:
+            response = generative_response(input)
+    return render_template('index.html', response=response)
 
-if (__name__== "__main__"):
+
+def generative_response(i):
+    response = model.generate_content(i)
+    return response.text
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-
